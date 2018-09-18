64eaa92ea6ab4b4f3d67ecaa712dd10a1727d0b9
Yihan Kim <kabi@kaist.ac.kr>
2018-09-18 13:54:36 +0900

    NN to KNN

--- main.py
+++ main.py
@@ -1,40 +1,89 @@
 
-# Ytr : 50000 x 1 int (0 .. 9)
-
+import math
 import numpy as np
-from NearestNeighbor import NearestNeighbor
 
-# constant
+# import knn model from "model.py"
+from model import KNearestNeighbor
 
-DATA_TOTAL = 10000 			# min 10000 ~ max 50000
-DATA_VALIDATE = 200
-DATA_TRAIN = DATA_TOTAL - DATA_VALIDATE
 
-# data reader
+### 1. Data reading
+
+# Data truncation constant
+
+#            DATA_TRAIN     TOTAL_READ
+#                 |  DATA_USE    |
+# 0               |     |        |
+# --------------------------------
+# |     TRAIN     | VAL |   ..   |
+# --------------------------------
+
+DATA_USE = 50000 			# max 50000
+DATA_VALIDATE = 100			# should be greater than 0, less than DATA_USE
+DATA_TRAIN = DATA_USE - DATA_VALIDATE
+
+DATA_BATCH_SIZE = 10000		# unit batch size
+
+# CIFAR-10 Data reader
+# (from https://www.cs.toronto.edu/~kriz/cifar.html)
 
 def unpickle(file):
+	'''
+	The archive contains the files data_batch_1, ..., data_batch_5, as well as test_batch. 
+	Each of these files is a Python "pickled" object produced with cPickle.
+	'''
 	import pickle
 	with open(file, 'rb') as fo:
 		dict = pickle.load(fo, encoding='bytes')
 	return dict
 
-Xtr_rows = []
-Ytr = []
+# Each data batch has 10,000 images recorded as 3,072(= 32 * 32 * 3) integers.
+# Read data as much as we need (<= 50000, determined by value of DATA_USE),
+# and save it(data and labels) to the X_rows and Y respectively.
 
-for i in range(1, 1 + DATA_TOTAL // 10000):
+X_rows = []
+Y = []
+
+for i in range(1, 1 + math.ceil(DATA_USE / DATA_BATCH_SIZE)):
 	data = unpickle("data/data_batch_%s" % str(i))
-	Xtr_rows += list(data[b'data'])
-	Ytr += list(data[b'labels'])
+	X_rows += list(data[b'data'])
+	Y += list(data[b'labels'])
 
-Xtr_rows = np.asarray(Xtr_rows)
-Ytr = np.asarray(Ytr)
+X_rows = np.asarray(X_rows)	
+Y = np.asarray(Y)
 
-cifar_nn = NearestNeighbor()
+# Each row vector in X contains 3,072 integer belongs to 0 to 255.
+# Y contains integer from 0 to 9, which indicates 10 classes in CIFAR-10.
+# if Y[i] == Y[j] for i != j, 
+# X[i] and X[j] represents the different object in same class(such as car).
 
-cifar_nn.train(Xtr_rows[:DATA_TRAIN], Ytr[:DATA_TRAIN])
-Ypred = cifar_nn.predict(Xtr_rows[DATA_TRAIN:])
 
-equal = [1 if Ypred[i] == Ytr[DATA_TRAIN + i] else 0 for i in range(DATA_VALIDATE)]
+### 2. Data splitting
 
-print(sum(equal) / DATA_VALIDATE)
+# Now, split the data and the labels into training set and validation set.
+# CIFAR-10 batch data has mixed order of class, so we don't need to shuffle it. 
+
+Xtr_rows = X_rows[:DATA_TRAIN]
+Ytr = Y[:DATA_TRAIN]
+
+Xval_rows = X_rows[DATA_TRAIN:DATA_USE]
+Yval = Y[DATA_TRAIN:DATA_USE]
+
+
+### 3. Running k-NN model
+
+# Use k-NN classifier for CIFAR-10
+
+cifar_knn = KNearestNeighbor() # kNN model defined in "model.py"
+cifar_knn.train(Xtr_rows, Ytr) # give training data into model
+validation_accuracies = []
+
+for k in [1, 3, 5, 10, 20, 50, 100]:
+	Yval_predict = cifar_knn.predict(Xval_rows, k = k)
+	acc = np.mean(Yval_predict == Yval)
+	print ('accuracy for k = %s: %f' % (k, acc))
+	# use a particular value of k and evaluation on validation data
+	validation_accuracies.append((k, acc))
+
+print("running results: ", end="")
+print(validation_accuracies)
 
---
+++ model.py
@@ -0,0 +1,66 @@
+
+import numpy as np
+
+# K-Nearest Neighbor Class
+# (from http://cs231n.github.io/classification/, modified slightly)
+
+class KNearestNeighbor(object):
+	'''
+	K-nearest Neighbor class
+	
+	train : Saves training data(lists of data and its labels). That's it.
+	predict : Consumes data list and parameter k, return list of predicted class number
+	'''
+	def __init__(self):
+		# do nothing
+		pass
+
+	def train(self, X, y):
+		""" 
+		X is N x D where each row is an example. 
+		Y is 1-dimension of size N 
+		return None
+		"""
+		# the nearest neighbor classifier simply remembers all the training data
+		self.Xtr = X
+		self.ytr = y
+		return
+
+	def predict(self, X, k = 1):
+		""" 
+		X is N x D where each row is an example we wish to predict label for.
+		k is hyperparameter; the number of neighbors that use to predict.
+		return list of class indices with length N.
+		"""
+		num_test = X.shape[0]
+		# lets make sure that the output type matches the input type
+		Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
+
+		# loop over all test rows
+		for i in range(num_test):
+			# find the nearest training image to the i'th test image
+			# using the L1 distance (sum of absolute value differences)
+			# You can change distance function as you need.
+			# -----------------------
+			distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+			# distances = np.sum(np.square(self.Xtr - X[i, :]), axis = 1)
+			# -----------------------
+			
+			nearest_indices = distances.argsort()[:k]
+			nearest_distances = distances[nearest_indices]
+			
+			# List "neighbor_label" have form [5, 4, 8, 5, 1] (when k = 5), 
+			# and function should return 5 in that case.
+			neighbor_labels = self.ytr[nearest_indices]
+			
+			# You have to implement strategy in case of neighbor_label have multiple majority.
+			# (ex. top 5 nearest neighbors are [3, 3, 4, 9, 4])
+			# select predictions from prediction list 
+
+			# implement kNN using nearest_indices and nearest_distances below.
+			# ----------------------
+			# it only returns first nearest element.
+			Ypred[i] = neighbor_labels[0]
+			# ----------------------
+
+		return Ypred
